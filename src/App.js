eef09dcbf6dc7be848339f79522edda58b5f5d4a
190030445Satishreddy <74000700+190030445Satishreddy@users.noreply.github.com>
2021-05-30 16:51:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Header from './Header.js';
+import Button from './Button.js';
+import Statistics from './Statistics.js';
+
+function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGoodFeedback = () => {
+    setGood(good + 1);
+  };
+
+  const addNeutralFeedback = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const addBadFeedback = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <Header text='Give feedback' />
+      <Button handleClick={addGoodFeedback} text='Good' />
+      <Button handleClick={addNeutralFeedback} text='Neutral' />
+      <Button handleClick={addBadFeedback} text='Bad' />
+      <Header text='Statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+export default App
---
+++ src/Statistics.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Statistic from './Statistic.js'
+
+const Statistics = (props) => {
+
+  const hasVotes = props.good + props.neutral + props.bad !==0
+
+  if(hasVotes) {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <Statistic text="Good" value={props.good}/>
+            <Statistic text="Neutral" value={props.neutral}/>
+            <Statistic text="Bad" value={props.bad}/>
+            <Statistic text="All" value={props.good + props.neutral + props.bad} />
+            <Statistic text="Average" value={((props.good * 1) + (props.bad * -1))/(props.good + props.neutral + props.bad)} />
+            <Statistic text="Positive" value={(props.good/(props.good + props.neutral + props.bad)) * 100 + '%'} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+  else {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+}
+
+export default Statistics;
